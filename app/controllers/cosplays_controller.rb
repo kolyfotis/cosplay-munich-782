c3c323d361e2a2416701de3ff0fa877b358f38a4
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-02-22 21:28:53 +0100

    cosplay routes, controller and temporary views

---
+++ app/controllers/cosplays_controller.rb
@@ -0,0 +1,48 @@
+class CosplaysController < ApplicationController
+  before_action :set_cosplay, only: %i[show edit update destroy]
+
+  def index
+    @cosplays = Cosplay.all
+  end
+
+  def show; end
+
+  def new
+    @cosplay = Cosplay.new
+  end
+
+  def create
+    @cosplay = Cosplay.new(cosplay_params)
+    @cosplay.user = current_user
+    if @cosplay.save
+      redirect_to cosplay_path(@cosplay)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @cosplay.update(cosplay_params)
+      redirect_to cosplay_path(@cosplay), notice: 'Your listing was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cosplay.destroy
+    redirect_to cosplays_path, notice: 'Your listing was successfully destroyed'
+  end
+
+  private
+
+  def cosplay_params
+    params.require(:cosplay).permit(:user_id, :price, :details)
+  end
+
+  def set_cosplay
+    @cosplay = Cosplay.find(params[:id])
+  end
+end
