abe3ace421f9a933675295c4c03386ee773eee4d
Fotios Kolytoumpas <57191836+kolyfotis@users.noreply.github.com>
2022-02-22 17:28:11 +0100

    length validation for last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true, length: { minimum: 2 }
-  validates :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true
+  validates :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true, length: { minimum: 2 }
 end
