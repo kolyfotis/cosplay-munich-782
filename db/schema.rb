d3afecedf6bc22dce3e4e95b86302951c6b6d09c
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-02-24 12:18:39 +0100

    merge with master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_202218) do
+ActiveRecord::Schema.define(version: 2022_02_24_102359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2022_02_23_202218) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "cosplay_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cosplay_id"], name: "index_bookings_on_cosplay_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "cosplays", force: :cascade do |t|
     t.float "price"
     t.text "details"
@@ -68,5 +78,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_202218) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "cosplays"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cosplays", "users"
 end
