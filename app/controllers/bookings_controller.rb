78375b5c55ca1122096dbe0885cff22146f13627
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-02-24 15:21:19 +0100

    finished with the booking controller, the routes and partially with the new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,28 +7,31 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @cosplay = Cosplay.find(params[:cosplay_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @cosplay = Cosplay.find(params[:cosplay_id])
+    @booking.cosplay = @cosplay
     @booking.user = current_user
     if @booking.save
       flash[:success] = "Your booking was created."
-      redirect_to bookings_path
+      redirect_to root_path
     else
       render :new
       flash[:warning] = "Your booking was not created"
     end
   end
 
-  def destroy
-    @booking.destroy
-    flash[:warning] = "Your booking has been deleted."
-  end
+  # def destroy
+  #   @booking.destroy
+  #   flash[:warning] = "Your booking has been deleted."
+  # end
 
   private
 
   def booking_params
-    params.require(:user).permit(:date, :user_id, :price, :details, :photo)
+    params.require(:booking).permit(:date)
   end
 end
