cdb4e45ffb54b01811a680665d422b9330efff26
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-02-24 14:18:09 +0100

    final stage of booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,16 +3,32 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+  def show; end
+
   def new
     @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new()
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      flash[:success] = "Your booking was created."
+      redirect_to bookings_path
+    else
+      render :new
+      flash[:warning] = "Your booking was not created"
+    end
   end
 
   def destroy
     @booking.destroy
-    redirect_to bookings_path, notice: "Your booking has been deleted."
+    flash[:warning] = "Your booking has been deleted."
+  end
+
+  private
+
+  def booking_params
+    params.require(:user).permit(:date, :user_id, :price, :details, :photo)
   end
 end
