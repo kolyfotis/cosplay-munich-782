0fb6583ceb319e734435db07bd5bbd4c1a2d471c
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-02-25 11:49:41 +0100

    title validation

--- app/models/cosplay.rb
+++ app/models/cosplay.rb
@@ -6,4 +6,5 @@ class Cosplay < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   # details to be between 6 and 255 characters long
   validates :details, presence: true, length: { in: 6..500 }
+  validates :title, presence: true, length: { in: 3..50 }
 end
