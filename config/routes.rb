8445352a0364c4151b91794d0b39c86552b321a8
Hasib Selimovic <95703159+Hasibius@users.noreply.github.com>
2022-02-25 11:07:41 +0100

    Revert "Calendar"

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cosplays do
-    resources :bookings, only: [ :show, :new, :create ]
+    resources :bookings, only: [ :new, :create ]
   end
 end
