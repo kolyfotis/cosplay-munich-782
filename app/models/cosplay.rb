afcee0d0582758b197f7a056172bef0c7de7800f
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-02-25 12:33:32 +0100

    price & photo validation

--- app/models/cosplay.rb
+++ app/models/cosplay.rb
@@ -3,8 +3,13 @@ class Cosplay < ApplicationRecord
   has_one_attached :photo
 
   # price to be positive
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true,
+                    format: { with: /\A\d+(\.\d{1,2})?\z/, message: "Invalid price format." }
   # details to be between 6 and 255 characters long
   validates :details, presence: true, length: { in: 6..500 }
   validates :title, presence: true, length: { in: 3..50 }
+  validates :photo, presence: true
+  # ,
+  #                   format: { with: /.+\.(png|jpg|jpeg)\z/,
+  #                             message: 'Invalid photo format. Only .png, .jpg and .jpeg extensions are allowed.' }
 end
